419e15e893ca36aac50853552dcc6dff93e4e8ef
daisybutler <daisybutler16@gmail.com>
2022-10-01 17:42:48 +0100

    Carousel overlay and button styling.

--- src/components/CarouselHome.js
+++ src/components/CarouselHome.js
@@ -5,6 +5,7 @@ const CarouselMain = () => {
   return (
     <Carousel variant="dark" id="carousel-home" controls={false} indicators={true} interval={2000}>
       <Carousel.Item>
+        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100"
           src="https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2087&q=80"
@@ -17,6 +18,7 @@ const CarouselMain = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100"
           src="https://images.unsplash.com/photo-1643185539104-3622eb1f0ff6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
@@ -29,6 +31,7 @@ const CarouselMain = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100"
           src="https://images.unsplash.com/photo-1619451427882-6aaaded0cc61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
