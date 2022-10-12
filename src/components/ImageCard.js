cce9f1fa11bb5b81227e21fea77cb8f7326c22b1
daisybutler <daisybutler16@gmail.com>
2022-10-12 21:03:21 +0100

    Pass src to ImageCard as props.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,10 @@
 import Card from 'react-bootstrap/Card';
 
-function ImageCard() {
+function ImageCard(src) {
+  console.log(src)
   return (
     <Card className="bg-dark text-white">
-      <Card.Img src="https://cdn.shopify.com/s/files/1/0565/1166/0080/files/Jack_Black_900x.jpg?v=1663068300" alt="Card image" />
+      <Card.Img src={src.src} alt="Card image" />
       <Card.ImgOverlay>
         <Card.Title>Item Title</Card.Title>
         <Card.Link className='underlined-link'>Shop Now</Card.Link>
--- src/templates/home/index.js
+++ src/templates/home/index.js
@@ -9,9 +9,9 @@ const Home = () => {
         <>
             <CarouselMain />
             <CardGroup id="image-cards-home" className="container">
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
+                <ImageCard src={"https://cdn.shopify.com/s/files/1/0565/1166/0080/files/Jack_Black_900x.jpg?v=1663068300"} />
+                <ImageCard src={"https://cdn.shopify.com/s/files/1/0565/1166/0080/files/Harrys_Razors_1100x.jpg?v=1663068708"} />
+                <ImageCard src={"https://cdn.shopify.com/s/files/1/0565/1166/0080/files/The_Ordinary_3_900x.jpg?v=1663069706"} />
             </CardGroup>
         </>
     )
