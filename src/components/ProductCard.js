8ec0ff4c553b7a66ba4518a703e2c5849db75880
daisybutler <daisybutler16@gmail.com>
2022-10-17 22:19:11 +0100

    Build out products page.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -24,17 +24,21 @@ function ProductCard() {
         return obj.category === "skincare";
     })
 
+    const formatPrice = (price) => {
+        let formattedPrice;
+        (typeof price === "number") ? formattedPrice = `£${price}.00` : formattedPrice = "Unavailable";
+        return formattedPrice;
+    }
+
     return (
         <>
         {filteredProducts.map((product, index) => 
-        <Card className='product-card' style={{ width: '18rem' }} id={`product_${product.id}`}>
-            <Card.Img variant="top" src={product.images[2]} />
+        <Card bsPrefix='product-card' className='col-12 col-sm-4 col-md-3' style={{ width: '16rem' }} id={`product_${product.id}`} key={product.id}>
+            <Card.Img variant="top" src={product.images[2]} style={{height: "300px", overflow: "hidden"}} />
             <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                {/* <Card.Text>
-                    {product.description}
-                </Card.Text> */}
-                <Button className='view-details-button'>View Details</Button>
+                <Card.Title className='product-title'>{product.title}</Card.Title>
+                <p className='product-price'>{formatPrice(product.price)}</p>
+                <Button bsPrefix='view-details-button' className='view-details-button'>+ Add To Cart</Button>
             </Card.Body>
         </Card>
         )}
--- src/templates/skincare/index.js
+++ src/templates/skincare/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ProductCard from "../../components/ProductCard";
+import { Row } from "react-bootstrap";
 
 const Skincare = () => {
     
     return (
       <>
         <h1>Skincare</h1>
-        <div className="container-fluid product-overview">
+        <div className="container product-overview">
+        <Row className="product-row">
           <ProductCard />
+        </Row>
+        <Row className="product-row">
           <ProductCard />
+        </Row>
         </div>
       </>
     )
