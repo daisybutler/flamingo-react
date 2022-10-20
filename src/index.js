7484a5d0e4440da0fff9894c2df09642603557d7
daisybutler <daisybutler16@gmail.com>
2022-10-20 21:20:31 +0100

    Installed FA as dependancy.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-
   <React.StrictMode>
     <BrowserRouter>
         <App />
